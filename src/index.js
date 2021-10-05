f632d48257ad5767d5f1a4b5da7c8c7ffba77e44
bilaluali <bilaluali98@gmail.com>
2021-10-05 18:31:52 +0200

    Added custom font

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './modules/core/redux/store';
+import theme from './modules/material-ui/theme';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/modules/material-ui/typography.js
@@ -0,0 +1,86 @@
+import { defaultFontColor } from './palette';
+
+export const baseFontFamily = ['Source Code Pro', 'sans-serif'];
+
+const typography = {
+    fontSize: 14,
+    h1: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '900',
+      color: defaultFontColor,
+    },
+    h2: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '900',
+      color: defaultFontColor,
+    },
+    h3: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '900',
+      color: defaultFontColor,
+    },
+    h4: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '900',
+      color: defaultFontColor,
+    },
+    h5: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '900',
+      color: defaultFontColor,
+    },
+    h6: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '900',
+      color: defaultFontColor,
+    },
+    subtitle1: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '600',
+      color: defaultFontColor,
+    },
+    subtitle2: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '600',
+      color: defaultFontColor,
+    },
+    body2: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '400',
+      color: defaultFontColor,
+    },
+    body1: {
+      fontFamily: baseFontFamily,
+      fontDisplay: 'swap',
+      fontWeight: '400',
+      color: defaultFontColor,
+    },
+    caption: {
+      fontFamily: baseFontFamily,
+      fontWeight: '400',
+      fontSize: '0.65rem',
+      textTransform: 'initial',
+      fontDisplay: 'swap',
+    },
+    button: {
+      fontFamily: baseFontFamily,
+      fontWeight: '900',
+      fontDisplay: 'swap',
+      textTransform: 'initial',
+    },
+    overline: {
+      fontFamily: baseFontFamily,
+      fontWeight: '600',
+    },
+}
+
+export default typography;
