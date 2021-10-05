c3fe6d5ead516ff7885ac63ff739728464e07bcd
bilaluali <bilaluali98@gmail.com>
2021-10-05 17:49:49 +0200

    Defined material-ui theme

---
+++ src/modules/material-ui/palette.js
@@ -0,0 +1,25 @@
+export const primaryColor = '#2C64B7';
+export const secondaryColor = '#252F37';
+export const successColor = '#199C77';
+export const errorColor = '#E02323';
+export const defaultFontColor = '#333333';
+
+const palette = {
+    primary: {
+      main: primaryColor,
+    },
+    secondary: {
+      main: secondaryColor,
+    },
+    success: {
+      main: successColor,
+    },
+    error: {
+      main: errorColor,
+    },
+    default: {
+      main: defaultFontColor,
+    },
+  }
+
+export default palette;
---
+++ src/modules/material-ui/theme.js
@@ -0,0 +1,7 @@
+import { createTheme } from '@mui/material/styles';
+
+import palette from './palette';
+
+export default createTheme({
+    palette,
+});
