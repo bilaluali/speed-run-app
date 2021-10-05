9f03ccdfc09be992257e936fbbb89d5352121bff
bilaluali <bilaluali98@gmail.com>
2021-10-05 21:03:05 +0200

    Defined GamesOverview component

---
+++ src/modules/game/components/GamesOverview/GamesOverview.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import {  Grid } from '@material-ui/core';
+import classnames from 'classnames';
+
+import GameCard from '../../containers/GameCard';
+import useStyles from './styles';
+
+const GamesOverview = ({
+    className,
+    items,
+    ...props
+}) => {
+    const classes = useStyles();
+    return (
+        <Grid
+            {...props}
+            className={classnames(className, classes.root)}
+            spacing={5}
+            container
+        >
+            {items.map(item => 
+                <Grid
+                    item
+                    key={item.id}
+                    xs={12}
+                    sm={4}
+                    md={4}
+                    lg={3}
+                >
+                    <GameCard {...item}/>
+                </Grid>
+            )}
+        </Grid>
+    )
+};
+
+export default GamesOverview;
