939ac6b90c2af824ef8b7ea56cbae4ca3f05fc11
bilaluali <bilaluali98@gmail.com>
2021-10-05 22:19:53 +0200

    Defined GamesOverview container

---
+++ src/modules/game/containers/GamesOverview.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+
+import GamesOverview from '../components/GamesOverview';
+import * as models from '../../models/redux';
+
+
+const GamesOverviewContainer = (props) => {
+    const games = useSelector(
+        state => models.game.selectors.filterBy(state, {})
+    );
+        
+    return (
+        <GamesOverview {...props} items={games}/>
+    );
+};
+
+export default GamesOverviewContainer;
