5d0808c7e73868417dd4679c04e2328c8b8dbe7f
bilaluali <bilaluali98@gmail.com>
2021-10-06 11:45:17 +0200

    Defined RunList container

---
+++ src/modules/run/containers/RunList.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+
+import RunList from '../components/RunList';
+import * as models from '../../models/redux';
+
+
+const RunListContainer = ({
+    game: gameId,
+    ...props
+}) => {
+    const runs = useSelector(
+        state => models.run.selectors.filterBy(state, { 
+            game: gameId
+        })[0]
+    );
+
+    const user = useSelector(
+        state => models.user.selectors.filterBy(state, { 
+            id: runs.users[0].id
+        })
+    );
+
+    const items = React.useMemo(() =>
+        runs.map(run => ({
+            primary: user.name,
+            secondary: run.time,
+            video: run.videos[0]
+        })), 
+        [runs, user]
+    );
+        
+    return (
+        <RunList {...props} items={items}/>
+    );
+};
+
+export default RunListContainer;
