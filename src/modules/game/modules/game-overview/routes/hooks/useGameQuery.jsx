6d5355440aa15a7a51a3712bc97bb61de2863c56
bilaluali <bilaluali98@gmail.com>
2021-10-06 12:29:42 +0200

    Defined useGameQuery

---
+++ src/modules/game/modules/game-overview/routes/hooks/useGameQuery.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { API_PATH } from '../../../../../api/config';
+import * as models from '../../../../../models/redux';
+
+export default function useGameQuery({
+    game: gameId
+}) {
+
+    const dispatch = useDispatch();
+
+    // NOTE: I just kept the side-effects simple, without using
+    // a redux side effect manager. App state was also kept with
+    // a simple architecture.
+    React.useEffect(() => {
+        fetch(`${API_PATH}/games/${gameId}`)
+            .then(response => {
+                if (response.ok)
+                    return response.json()
+                throw response
+            })
+            .then(res => {
+                dispatch(
+                    models.game.actionsCreators.addEntities(res.data)
+                )
+            })
+            .catch(error => {
+                console.log("ERR_FETCH: Cannot fetch data");
+            })
+    }, [dispatch]);
+
+};
