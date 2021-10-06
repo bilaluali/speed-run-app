493641a53e5af34b47eefbbf8b2825afe7333fec
bilaluali <bilaluali98@gmail.com>
2021-10-06 11:17:54 +0200

    Defined RunListItem component

---
+++ src/modules/run/components/RunListItem/RunListItem.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { ListItem, ListItemText, IconButton } from '@material-ui/core';
+import VideoIcon from '@mui/icons-material/OndemandVideoIcon';
+import classnames from 'classnames';
+
+import useStyles from './styles';
+
+const RunListItem = ({
+    className,
+    primary,
+    secondary,
+    video,
+    ...props
+}) => {
+    const classes = useStyles();
+    return (
+        <ListItem
+            {...props}
+            className={classnames(className, classes.root)}
+            secondaryAction={
+                <IconButton
+                    edge="end"
+                    aria-label="video"
+                    href={video}
+                >
+                    <VideoIcon />
+                </IconButton>
+            }
+
+        >
+            <ListItemText
+                className={classes.listItemText}
+                primary={primary}
+                secondary={secondary}
+                primaryTypographyProps={
+                    React.useMemo(() => ({ variant: 'body1' }), [])
+                }
+                secondaryTypographyProps={
+                    React.useMemo(() => ({ variant: 'body1' }), [])
+                }
+            />
+        </ListItem>
+    )
+};
+
+export default RunListItem;
